8975d8635442739edb2e1d7e2252476ca7e434fb
Piotr Woloszun <piotr.woloszun@gmail.com>
2018-06-18 15:12:50 +0200

    v1.2.1

--- src/es-tasks/es6-features/01-advanced-modules/shapes-task.js
+++ src/es-tasks/es6-features/01-advanced-modules/shapes-task.js
@@ -28,8 +28,8 @@ const circle = {
 export function shapesTaskApp() {
 // Do not edit below this line
 //   console.log(isIsosceles(3, 7, 7) === true);
-//   console.log(triangle.area(4, 7) === 14);
-//   console.log(square.area(2, 2) === 4);
-//   console.log(circle.area(4.7) === 69.39778171779854);
-//   console.log(diameter(4.7) === 9.4);
+  console.log(triangle.area(4, 7) === 14);
+  console.log(square.area(2, 2) === 4);
+  console.log(circle.area(4.7) === 69.39778171779854);
+  // console.log(diameter(4.7) === 9.4);
 }
