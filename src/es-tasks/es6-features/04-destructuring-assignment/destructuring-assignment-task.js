8640bf41d5cfc06babb89e04b4a9d58f460e44ef
Piotr Woloszun <piotr.woloszun@gmail.com>
2018-06-19 10:41:33 +0200

    v1.2.1

--- src/es-tasks/es6-features/04-destructuring-assignment/destructuring-assignment-task.js
+++ src/es-tasks/es6-features/04-destructuring-assignment/destructuring-assignment-task.js
@@ -1,26 +1,12 @@
-// The detectCollision function searches through an array of rectangles and
-// returns the first rectangle that the given point is inside of.
-
-// Use destructuring and a higher-order function to make this code cleaner.
-// You might want to use the new array method find, which takes a function as
-// argument, and returns the first element in the array (the element, not its
-// index) for which the function returns true.
-
-// TODO: use destructuring
+// TODO: use destructuring:
+// object -> const destruct
+// point -> parameter destruct
 function doesCollideWith(object, point) {
 // TODO
-}
-
-// TODO: use for-of
-// TODO: re-use doesCollideWith
-function detectCollision(objects, point) {
-  for (let i = 0; i < objects.length; i++) {
-    const object = objects[i];
-    if (point.x >= object.x && point.x <= object.x + object.width &&
-      point.y >= object.y && point.y <= object.y + object.height) {
-      return object;
-    }
-  }
+  return point.x >= object.x &&
+    point.x <= object.x + object.width &&
+    point.y >= object.y &&
+    point.y <= object.y + object.height;
 }
 
 function testDetectCollision() {
@@ -30,7 +16,9 @@ function testDetectCollision() {
     { x: 0, y: 0, width: 10, height: 5 }
   ];
 
-  console.log(detectCollision(myObjects, { x: 4, y: 2 }));
+  for (let obj of myObjects) {
+    console.log('testDetectCollision', doesCollideWith(obj, { x: 4, y: 2 }));
+  }
 }
 
 // TODO 2
