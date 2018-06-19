0b6f461c214be9a106e77497a8399129cc28c1c9
Piotr Woloszun <piotr.woloszun@gmail.com>
2018-06-19 09:11:42 +0200

    v1.2.1

--- src/es-tasks/reusable-code/timer-app/src/Timer.js
+++ src/es-tasks/reusable-code/timer-app/src/Timer.js
@@ -1,3 +1,11 @@
+const intervalId = setInterval(function () {
+  // this === window/undefined
+  console.log('a qq!', this);
+}, 2000);
+
+clearInterval(intervalId);
+
+
 // TODO: impl Timer class, which inherits from Evented
 
 export function Timer() { // TODO: remove
